bb1b7ef0abe034f23d88b957814fab9c634b718a
Long Lê Đăng <hicongtuoc@gmail.com>
2024-06-30 18:11:49 +0700

    feat: create ui basic

---
+++ app/(apps)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard App</h1>
+      <p>This is the dashboard app. It's a great place to see dashboards.</p>
+    </div>
+  )
+}
---
+++ app/(marketing)/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Marketing App</h1>
+      <p>This is the marketing app. It's a great place to market things.</p>
+    </div>
+  )
+}
